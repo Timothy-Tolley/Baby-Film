6f8e99e8c0489f25905cc315a2428f6837bde516
timothy-tolley <timothytolley@outlook.com>
2018-03-23 08:49:31 +1300

    fixed porting

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 const server = require('./server')
 
-const port = process.env.NODE_ENV || 3000
+const port = process.env.PORT || 3000
 
 server.listen(port, () => {
   // eslint-disable-next-line no-console
