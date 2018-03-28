3eb7d1c0d1a58dc6c64a9691926c674537d3a67a
timothy-tolley <timothytolley@outlook.com>
2018-03-28 21:56:55 +1300

    responsiveness fixed

--- client/components/DetachedHead/DetachedHead.jsx
+++ client/components/DetachedHead/DetachedHead.jsx
@@ -7,21 +7,21 @@ import './detached-head.css'
 class DetachedHead extends React.Component {
   render () {
     return (
-      <div className = 'header-container'>
-        <Link to ={'/#synopsis'} className = 'header-link'>
-          <p className = 'header-link'> SYNOPSIS </p>
+      <div className = 'det-header-container'>
+        <Link to ={'/#synopsis'} className = 'det-header-link'>
+          <p className = 'det-header-link'> SYNOPSIS </p>
         </Link>
-        <Link to='/#gallery' className = 'header-link'>
-          <p className = 'header-link'> GALLERY </p>
+        <Link to='/#gallery' className = 'det-header-link'>
+          <p className = 'det-header-link'> GALLERY </p>
         </Link>
-        <Link to = {'/'} className = 'logo-image-cont'>
-          <img src = '/images/baby-logo-white.png' className = 'logo-image'/>
+        <Link to = {'/'} className = 'det-logo-image-cont'>
+          <img src = '/images/baby-det-logo-white.png' className = 'det-logo-image'/>
         </Link>
-        <Link to='/#production' className = 'header-link'>
-          <p className = 'header-link'> PRODUCTION </p>
+        <Link to='/#production' className = 'det-header-link'>
+          <p className = 'det-header-link'> PRODUCTION </p>
         </Link>
-        <Link to='/#contact' className = 'header-link'>
-          <p className = 'header-link'> CONTACT </p>
+        <Link to='/#contact' className = 'det-header-link'>
+          <p className = 'det-header-link'> CONTACT </p>
         </Link>
       </div>
     )
