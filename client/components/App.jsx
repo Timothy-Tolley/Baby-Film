b9999ef8b6e559eaa4a99851b481800c75a7908a
timothy-tolley <timothytolley@outlook.com>
2018-06-01 11:31:44 +1200

    should be working

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,20 +2,12 @@ import React from 'react'
 import {Route} from 'react-router-dom'
 
 import Home from './Home/Home'
-import Cast from './Cast/Cast'
-import Faqs from './Faqs/Faqs'
-import Director from './Director/Director'
-import Producers from './Producers/Producers'
 
 class App extends React.Component {
   render () {
     return (
       <div className = 'page'>
         <Route exact path = '/' component = {Home}/>
-        <Route exact path = '/cast' component = {Cast} />
-        <Route exact path = '/faqs' component = {Faqs} />
-        <Route exact path = '/director' component = {Director} />
-        <Route exact path = '/producers' component = {Producers} />
       </div>
     )
   }
