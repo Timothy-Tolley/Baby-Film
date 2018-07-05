ba12ea09a8df49db44843edca5805f8d5f6dd2dc
timothy-tolley <timothytolley@outlook.com>
2018-07-05 15:06:17 +1200

    made changes

--- client/components/About/About.jsx
+++ client/components/About/About.jsx
@@ -12,9 +12,14 @@ class About extends React.Component {
         <div className = 'about-content-cont'>
           <h2 className = 'synopsis-header'> SYNOPSIS </h2>
           <p className = 'synopsis-text'>
-            {"Isabelle is determined not to spend her 65th birthday alone. When her daughter doesn't turn up for dinner, Isabelle invites herself into the home of her newneighbour, Leisha."}
-            <br/>
-            {"Once inside and eating cake, Isabelle soon discovers that thisyoung woman has recently lost her baby to cot death. When Isabelle's prying triggersa nervous breakdown in Leisha, she helps her in a way nobody else can."}
+            {"Isabelle is determined not to spend her 65th birthday alone. When her daughter doesn't turn up for dinner, Isabelle invites herself into the home of her new neighbour, Leisha."}
+
+          </p>
+          <p className = 'synopsis-text'>
+            {'Once inside and eating cake, Isabelle soon discovers that this young woman has recently lost her baby to cot death.'}
+          </p>
+          <p className = 'synopsis-text'>
+            {"When Isabelle's prying triggers a nervous breakdown in Leisha, she helps her in a way nobody else can."}
           </p>
         </div>
       </div>
--- client/components/Director/Director.jsx
+++ client/components/Director/Director.jsx
@@ -8,15 +8,16 @@ class Director extends React.Component {
       <div className = 'director-container'>
         <div className = 'director-half'>
           <img src = '/images/mark-prebble.jpg' className = 'director-image'/>
+          <p className = 'director-blurb'> Mark Prebble </p>
         </div>
         <div className = 'director-half'>
-          <h2 className = 'director-header'> DIRECTOR - MARK PREBBLE </h2>
+          <h2 className = 'director-header'>{ "DIRECTOR'S STATEMENT"} </h2>
           <p className = 'director-text'>
-            Baby? is a heartfelt and funny drama about two mothers connecting. We were inspired to write this story after our first daughter was born. Some things we wanted to talk about were: postnatal depression, the need for community and a story that celebrates older women. We also wanted a project in which mothers of young children could take key creative roles. We hope Baby? willdraw our audience in with warmth and humour. We want to encourage people to talk about mental health and basically make them want call their mums.
+            Baby? is a heartfelt and funny drama about two mothers connecting. We were inspired to write this story after our first daughter was born. Some things we wanted to talk about were: postnatal depression, the need for community and a story that celebrates older women. We also wanted a project in which mothers of young children could take key creative roles. We hope Baby? will draw our audience in with warmth and humour. We want to encourage people to talk about mental health and basically make them want call their mums.
           </p>
           <h2 className = 'director-header'> DIRECTOR’S BIOGRAPHY </h2>
           <p className = 'director-text'>
-           Mark Prebble Mark has written and directed film and theatre projects in UK, Australia and around New Zealand. His play The Pantry Shelf, co-written with wife Marion, was an award-winner in Edinburgh and Auckland and a finalist for Best Play Script at Moondance Film Festival. His 2005 feature film Futile Attraction was the first feature film completed using internet crowd-funding. This was funded via his website www.MakeMarksMovie.com (now offline) which predates Facebook and Kickstarter.
+           Mark has written and directed film and theatre projects in UK, Australia and around New Zealand. His play The Pantry Shelf co-written with wife, Marion, was an award-winner in Edinburgh and Auckland and a finalist for Best Play Script at Moondance Film Festival. His 2005 feature film Futile Attraction was the first feature film completed using internet crowd-funding. This was funded via his website www.MakeMarksMovie.com (now offline) which predates Facebook and Kickstarter.
           </p>
         </div>
       </div>
